f1d3aba1f7c6eeda49d4ef864f80e1f23a998d4e
송준형 <ssong99999@naver.com>
2025-03-10 11:14:25 +0900

    🎉 EDA-java init

---
+++ eda-java/src/test/java/com/in4sight/eda/EdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in4sight.eda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
