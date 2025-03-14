8ddc87a8f1c709fbe43584581815f5140244e214
김형표 <jmn9798@naver.com>
2025-03-14 17:03:20 +0900

    ✨ [JIRA-S12P21S004-119] 문 열림 이벤트 감지 로직 추가

--- das/app/sensor.py
+++ das/app/sensor.py
@@ -6,8 +6,8 @@ import json
 
 from influxdb_client import InfluxDBClient
 
-from app.config import (INFLUXDB_BUCKET_SENSOR, INFLUXDB_ORG, INFLUXDB_TOKEN,
-                        INFLUXDB_URL)
+from app.config import (INFLUXDB_BUCKET_EVENT, INFLUXDB_BUCKET_SENSOR,
+                        INFLUXDB_ORG, INFLUXDB_TOKEN, INFLUXDB_URL)
 
 # influxdb 연결
 client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
@@ -16,7 +16,7 @@ client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG
 query_api = client.query_api()
 
 
-def get_refrigerator_sensors_data():
+def get_refrigerator_sensors_data(serial_number):
     """
     influxDB에서 모든 냉장고 시계열 데이터 가져오는 함수  (냉장실 + 냉동실)
     """
@@ -30,19 +30,28 @@ def get_refrigerator_sensors_data():
     result = {}
 
     # (현재 기준 3시간 전 데이터 ~ 지금까지 데이터)
-    query = f"""
+    sensors_query = f"""
     from(bucket: "{INFLUXDB_BUCKET_SENSOR}")
     |> range(start: -3h)
-    |> filter(fn: (r) => r._measurement == "refrigerator")
+    |> filter(fn: (r) => r._measurement == "refrigerator" and r.serial_number == "{serial_number}")
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+    |> yield()
+    """
+
+    event_query = f"""
+    from(bucket: "{INFLUXDB_BUCKET_EVENT}")
+    |> range(start: -3h)
+    |> filter(fn: (r) => r._measurement == "refrigerator" and r.serial_number == "{serial_number}")
     |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
     |> yield()
     """
 
     # 쿼리 실행 후 pandas DataFrame으로 변환
-    df = query_api.query_data_frame(org=INFLUXDB_ORG, query=query)
+    df_sensor = query_api.query_data_frame(org=INFLUXDB_ORG, query=sensors_query)
+    df_event = query_api.query_data_frame(org=INFLUXDB_ORG, query=event_query)
 
     # 온도 관련 이상치 감지
-    temp_anomalies, temp_sensors = detect_temperature_anomalies(df)
+    temp_anomalies, temp_sensors = detect_temperature_anomalies(df_sensor, df_event)
     anomaly_prompts.extend(temp_anomalies)
     related_sensor.extend(temp_sensors)
 
@@ -64,7 +73,7 @@ def get_refrigerator_sensors_data():
     return formatted_json
 
 
-def detect_temperature_anomalies(df):
+def detect_temperature_anomalies(df, df_event):
     """
     온도 관련 이상 감지 로직
     """
@@ -103,4 +112,26 @@ def detect_temperature_anomalies(df):
         if not high_anomaly_temp_external.empty:
             anomaly_prompts.append("외부 온도 이상 고온 감지")
 
+    if "event_type" in df_event.columns:
+
+        # 문 열림 이벤트
+        door_open_data = df_event[df_event["event_type"] == "door_open"]
+
+        if not door_open_data.empty:
+
+            # 가장 최근 문이 열린 시간
+            last_door_open_time = door_open_data["_time"].max()
+
+            # 지금 문이 열려있는 상태인지 판단
+            door_close_data = df_event[
+                (df_event["event_type"] == "door_close")
+                & (df_event["_time"] > last_door_open_time)
+            ]
+
+            if door_close_data.empty:
+                anomaly_prompts.append("문이 열려있습니다.")
+
     return anomaly_prompts, related_sensor
+
+
+get_refrigerator_sensors_data(1234)
