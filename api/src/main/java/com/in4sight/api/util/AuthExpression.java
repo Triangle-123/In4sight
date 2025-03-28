ca31ebbbcb2531f1dc16352bad61f87a6facb375
송준형 <ssong99999@naver.com>
2025-03-28 11:02:35 +0900

    ✨ [JIRA-S12P21S004-141] AllowIpRepository로 허용 IP list 구분

--- api/src/main/java/com/in4sight/api/util/AuthExpression.java
+++ api/src/main/java/com/in4sight/api/util/AuthExpression.java
@@ -1,16 +1,17 @@
 package com.in4sight.api.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.in4sight.api.repository.AllowIpRepository;
 
 @Component
 public class AuthExpression {
-	private final List<String> preAllowedList;
+	private final AllowIpRepository allowIpRepository;
 
-	public AuthExpression() {
-		preAllowedList = new ArrayList<>();
+	@Autowired
+	public AuthExpression(AllowIpRepository allowIpRepository) {
+		this.allowIpRepository = allowIpRepository;
 	}
 
 	/**
@@ -29,6 +30,6 @@ public class AuthExpression {
 	 * @return 인가 여부
 	 */
 	public boolean matchIpByAllowedList(String ip) {
-		return preAllowedList.contains(ip);
+		return allowIpRepository.contains(ip);
 	}
 }
