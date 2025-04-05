7f17faaaab19989b23bd1ea62c7bb4fb91816b55
cinador <cinador3@gmail.com>
2025-04-06 01:19:08 +0900

    🐛 [JIRA-S12P21S004-254] 이벤트 데이터 대응되도록 변경

--- das/app/api_ref_data_sending.py
+++ das/app/api_ref_data_sending.py
@@ -129,12 +129,15 @@ def event_summary(df_event):
 
     for event in SIMPLE_EVENT_DATA_LIST:
         event_data = {}
+        target_df_event = df_event[df_event["event_type"] == event]
+
         event_data["field"] = f"{event}"
         event_data["measurement"] = f"{METRICS[event]}"
         event_data["time"] = [
             d.strftime("%Y-%m-%d %H:%M:%S") for d in target_df_event["_time"]
         ]
 
+        print(event_data)
         event_dataset.append(event_data)
 
     return event_dataset
